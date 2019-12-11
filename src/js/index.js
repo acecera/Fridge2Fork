99445fca0f64df1ffb576fa2dff2df2dbb069752
Anthony Cera <ace_cera@yahoo.com>
2019-12-10 18:16:21 -0800

    installed a different version of babel to fix error message I was getting when running dev script. Fixed the syntaxin webpack.config file.

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,5 @@
 // Global app controller
 import exp from './test';
+const x = 24;
 
-console.log(`I imported ${exp} from test.js!`)
+console.log(`I imported ${exp} from test.js! Variable x is ${x}`)
