54980d331f16ef286240131f1e6bc0ff5834715a
Anthony Cera <ace_cera@yahoo.com>
2020-02-20 21:56:00 -0800

    downloaded the axios package. Added search constructor. Added the logic to search for recipes.

--- src/js/index.js
+++ src/js/index.js
@@ -1,5 +1,5 @@
-// Global app controller
-import exp from './test';
-const x = 24;
+import Search from './models/Search';
 
-console.log(`I imported ${exp} from test.js! Variable x is ${x}`)
+const search = new Search('pizza');
+console.log(search);
+search.getResults();
